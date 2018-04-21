5a12f26729c76d2dc0e99efd44b2dd9ad531b321
Jean ROMAGNANI <jromagna@e1r12p5.42.fr>
2018-04-21 22:49:12 +0200

    ouais

---
+++ Assets/Scripts/GodEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodEvent : MonoBehaviour {
+
+
+	// Use this for initialization
+	public static event BuildFarm farmEvent;
+
+
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (  )
+			farmEvent();
+	}
+}
+
