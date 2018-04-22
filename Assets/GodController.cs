e7f358debbed4fc778e23cdb16a50f3ed1d919c8
alelievr <alelievr@student.42.fr>
2018-04-22 19:27:11 +0200

    Added simple move controller

---
+++ Assets/GodController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GodController : MonoBehaviour
+{
+	public float	speed;
+
+	NavMeshAgent	agent;
+
+	void Start ()
+	{
+		agent = GetComponent< NavMeshAgent >();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+		agent.Move(input.normalized * speed);
+	}
+}
