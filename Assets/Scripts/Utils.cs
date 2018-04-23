cb67ac2382d7b171bd37b8fe71ffa85dca50a9f7
Gabriel FOURNIER <gabfou95@gmail.com>
2018-04-23 17:22:54 +0200

    fdsf

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils : MonoBehaviour {
+
+	public Material soldatmat;
+	public GodEvent godEvent;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
