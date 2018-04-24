0c5c8e144704f128c5da79ae30caa7053be6cd9b
alelievr <alelievr@student.42.fr>
2018-04-24 04:08:43 +0200

    Tutorial load scene

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,12 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class Tutorial : MonoBehaviour
 {
+	bool		win = false;
+
 
 	void Start ()
 	{
 		GameManager.instance.AddCrystal(300);
+	}
+
+	private void Update()
+	{
+		if (GodEvent.listAllBadGuys.All(b => b == null) || GodEvent.listAllBadGuys.Count == 0)
+		{
+			GameManager.instance.Win();
+			win = true;
+		}
+
+		if (win && Input.GetKeyDown(KeyCode.Return))
+			SceneManager.LoadScene("level 1");
 	}
 }
